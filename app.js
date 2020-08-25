1916d4e2cfd6b7ffda1a1f8cc0dead4a4656abab
Manish Poduval <manishpoduval@hotmail.com>
2020-08-25 23:36:13 +0530

    deploy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function renderImage(){
     element.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg"/>`
 }
 
-let minusElement = document.getElementById('minus')
+let minusElement = document.getElementById('prev')
 minusElement.onclick = function(){
     if (count > 1){
         count = count - 1;
@@ -13,7 +13,7 @@ minusElement.onclick = function(){
     }
 }
 
-let plusElement = document.getElementById('plus')
+let plusElement = document.getElementById('next')
 plusElement.onclick = function(){
     if (count < 650){
         count = count + 1;
